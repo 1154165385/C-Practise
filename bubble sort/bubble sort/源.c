08a272e37551780a00bd6d8145561389d0ad65b6
1154165385 <1154165385@qq.com>
2021-08-13 16:13:59 +0800

    start to learn c

---
+++ bubble sort/bubble sort/源.c
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+
+void bubble_sort(int arr[],int x)
+{
+	int a, b, d;
+	for (a = 0; a <x-1; a++)
+	{
+		for (b = 0; b < x-1-a; b++)
+		{
+			if (arr[b] > arr[b + 1])
+			{
+				d = arr[b+ 1];
+				arr[b + 1] = arr[b];
+				arr[b] = d;
+			};;
+		};
+	};
+
+}
+
+int main()
+{
+	int arr1[] = { 9,5,7,6,8,2,4,3,1 }, sz, i;
+	sz = sizeof(arr1) / sizeof(arr1[0]);
+	bubble_sort(arr1, sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr1[i]);
+	};
+}
